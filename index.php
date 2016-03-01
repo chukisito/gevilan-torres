23c680a0fa4f887a8c521b9f718849204376a21f
edith espinoza <edith_espino@hotmail.com>
2016-02-29 19:48:59 -0500

    hoy aprobare el examen

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>lynn</title>
+	<link rel="stylesheet" href="">
+
+</head>
+<body>
+<marquee>
+<h1>lynn gavilan torres</h1>
+</marquee>
+<img src="imagen.1.jpg"
+	
+</body>
+</html>
